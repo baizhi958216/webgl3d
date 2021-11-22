032c87d12623160f73354da4990a45fabf44eeaa
baizhi958216 <1475289190@qq.com>
2021-11-22 22:56:01 +0800

    "App：重设导航"

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,6 @@
         <el-icon size="15px"><open /></el-icon>
         <span>XR虚拟摄影 demo</span>
       </el-button>
-      <!-- Logo -->
       <el-button type="text">
         <span>关于</span>
       </el-button>
@@ -13,10 +12,14 @@
     <!-- 顶导航栏 -->
 
     <el-container>
-      <el-aside style="width: 200px">
+      <el-aside style="width: 300px">
         <div class="demo-collapse">
-          <el-collapse v-model="activeNames" @change="handleChange">
+          <el-collapse v-model="activeNames" @change="handleChange" style="width:  220px">
             <el-collapse-item title="演示" name="1">
+              <el-radio-group v-model="radio">
+                <el-radio :label="1">3D</el-radio>
+                <el-radio :label="2">2D</el-radio>
+              </el-radio-group>
               <!-- <div>啥都没</div> -->
             </el-collapse-item>
             <el-collapse-item title="模型" name="2">
@@ -29,11 +32,31 @@
               <!-- <div>啥都没</div> -->
             </el-collapse-item>
             <el-collapse-item title="场景" name="5">
+              <el-select v-model="value" placeholder="在下拉菜单中选择">
+                <el-option
+                  v-for="item in options"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value"
+                >
+                </el-option>
+              </el-select>
               <!-- <div>啥都没</div> -->
             </el-collapse-item>
-            <el-collapse-item title="重置" name="6">
-              <!-- <div>啥都没</div> -->
-            </el-collapse-item>
+            <!-- 重置 -->
+            <el-tooltip content="还原场景" placement="right" effect="light">
+              <el-button
+                plain
+                @click="openCenter"
+                style="
+                  margin: 20px 0 20px 0;
+                  position: relative;
+                  left: 50%;
+                  transform: translateX(-50%);
+                "
+                >重置</el-button
+              >
+            </el-tooltip>
           </el-collapse>
         </div>
       </el-aside>
@@ -56,7 +79,8 @@ import * as THREE from "three";
 import * as Stats from "stats.js";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { Open } from "@element-plus/icons";
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
+import { ElMessage } from "element-plus";
 // import { ref } from 'vue'
 export default defineComponent({
   name: "App",
@@ -67,13 +91,34 @@ export default defineComponent({
     Open,
   },
   setup() {
-    // const activeNames = ref(['1'])
-    const handleChange = (val) => {
-      console.log(val);
+    const activeNames = ref(["1"]);
+    const openCenter = () => {
+      ElMessage({
+        showClose: true,
+        message: "成功",
+        center: true,
+        type: "success",
+      });
     };
+    // const handleChange = (val) => {
+    //   console.log(val);
+    // };
     return {
-      // activeNames,
-      handleChange,
+      activeNames,
+      openCenter,
+      radio: ref(1),
+      options: ref([
+        {
+          value: "Option1",
+          label: "场景一",
+        },
+        {
+          value: "Option2",
+          label: "场景二",
+        },
+      ]),
+      value: ref(""),
+      // handleChange,
     };
   },
   methods: {
@@ -91,7 +136,7 @@ export default defineComponent({
         0.1,
         1000
       );
-      camera.position.set(1, 1, 1);
+      camera.position.set(10, 10, 10);
       // 渲染器
       const renderer = new THREE.WebGLRenderer({
         canvas,
@@ -101,31 +146,30 @@ export default defineComponent({
       // 创建一个大盒子cube
       const geometry = new THREE.BoxGeometry(120 * 5, 100 * 5, 100 * 5);
       // 创建一个小球littleGeometry
-      // const littleGeometry = new THREE.IcosahedronGeometry(1, 60, 1);
+      const littleGeometry = new THREE.IcosahedronGeometry(1, 60, 1);
 
       // 载入贴图
       const textureloader = new THREE.TextureLoader();
       const texture = textureloader.load(
-        // "https://uploadstatic.mihoyo.com/contentweb/20191114/2019111412090396621.png"
         "https://uploadstatic.mihoyo.com/contentweb/20200410/2020041019014847737.png"
       );
       const material = new THREE.MeshBasicMaterial({
         map: texture,
         side: THREE.DoubleSide,
       });
-      // const textureloader2 = new THREE.TextureLoader();
-      // const texture2 = textureloader2.load(
-      //   "https://uploadstatic.mihoyo.com/contentweb/20200410/2020041019014847737.png"
-      // );
-      // const material2 = new THREE.MeshBasicMaterial({
-      //   map: texture2,
-      //   side: THREE.DoubleSide,
-      // });
+      const textureloader2 = new THREE.TextureLoader();
+      const texture2 = textureloader2.load(
+        "https://uploadstatic.mihoyo.com/contentweb/20191114/2019111412090396621.png"
+      );
+      const material2 = new THREE.MeshBasicMaterial({
+        map: texture2,
+        side: THREE.DoubleSide,
+      });
       const cube = new THREE.Mesh(geometry, material);
-      // const littlecube = new THREE.Mesh(littleGeometry, material);
+      const littlecube = new THREE.Mesh(littleGeometry, material2);
       // 在场景加入创建的盒子cube
       scene.add(cube);
-      // scene.add(littlecube);
+      scene.add(littlecube);
       // FPS显示
       const stats = new Stats();
       stats.setMode(0);
@@ -165,18 +209,20 @@ export default defineComponent({
 }
 .demo-collapse {
   margin-top: 20px;
-  text-align: center;
+  /* text-align: center; */
   display: flex;
   justify-content: center;
   user-select: none;
+  align-items: center;
 }
 #canvas {
   height: 100%;
   width: 100%;
 }
-#modelbox,#three {
-  width: 750px;
-  height: 500px;
+#modelbox,
+#three {
+  width: 1000px;
+  height: 600px;
   position: relative;
   left: 50%;
   top: 50%;
